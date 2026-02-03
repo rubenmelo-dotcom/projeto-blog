503e1f8d3293d6c77a6927225a0add5996bc03e3
rubenmelo-dotcom <pinguim6970@gmail.com>
2026-02-02 21:41:58 -0300

    Até a aula 551

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,16 +1,12 @@
+from blog.models import Post
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from blog.models import Post
 
 PER_PAGE = 9
 
 
 def index(request):
-    posts = (
-        Post
-        .objects
-        .get_published()
-    )
+    posts = Post.objects.get_published()
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -25,11 +21,41 @@ def index(request):
     )
 
 
-def page(request):
-    paginator = Paginator(posts, 9)
+def created_by(request, author_pk):
+    posts = Post.objects.get_published()\
+        .filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -39,15 +65,17 @@ def page(request):
     )
 
 
-def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+def post(request, slug):
+    post = (
+        Post.objects.get_published()
+        .filter(slug=slug)
+        .first()
+    )
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
